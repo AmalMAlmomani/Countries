e21c9740cde5b469adb7318101e503722e8e3289
amal almomani <amalalmomani19@gmail.com>
2020-02-06 10:25:30 +0200

    d

--- javascript/addcontnt.js
+++ javascript/addcontnt.js
@@ -9,9 +9,9 @@ var showOrder = function(){
     for (var i = 0 ; i < numberOfImages ; i++){
         console.log("index is " + i);
         if ( userOrder === "hotel"){
-            orderImage = orderImage + '<img src="img/hotel.png">';
+            orderImage = orderImage + '<img src="img/https://previews.123rf.com/images/sarahdesign/sarahdesign1706/sarahdesign170600677/80760671-hotel-icon.jpg">';
         }else if ( userOrder === "house"){
-            orderImage = orderImage + '<img src="img/house.jpg">';
+            orderImage = orderImage + '<img src="img/https://www.pngitem.com/pimgs/m/76-765395_house-png-icon-house-icon-for-resume-transparent.png">';
         }console.log(orderImage);
        
      }return orderImage;
