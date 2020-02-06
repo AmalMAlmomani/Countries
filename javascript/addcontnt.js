066824a8347cb831d3d1f83f5d7bcd42b1c5af41
amal almomani <amalalmomani19@gmail.com>
2020-02-06 10:03:24 +0200

    change

---
+++ javascript/addcontnt.js
@@ -0,0 +1,20 @@
+var showOrder = function(){
+
+    var userOrder ;
+    var numberOfImages;
+    var orderImage = '';
+    while ( userOrder !== "house" && userOrder !== "hotel"){
+        userOrder = prompt(" please enter house or hotel.")
+    numberOfImages = prompt("how many images do you want ?");
+    for (var i = 0 ; i < numberOfImages ; i++){
+        console.log("index is " + i);
+        if ( userOrder === "hotel"){
+            orderImage = orderImage + '<img src="images/hotel.png">';
+        }else if ( userOrder === "house"){
+            orderImage = orderImage + '<img src="images/house.png">';
+        }console.log(orderImage);
+       
+     }return orderImage;
+    }
+    }
+    document.write('<p>' + showOrder() + '</p>');
